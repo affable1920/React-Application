1f71652b9454984b591d6e68fd5e869089c257ca
Shamik Zaffar <affableshamik98@gmail.com>
2024-12-14 23:13:39 +0530

    Created a custom reusable mutation hook

---
+++ src/hooks/useAddTasks.jsx
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { addDoc, collection } from "firebase/firestore";
+import auth from "../services/auth";
+
+const useAddTasks = () => {
+  const history = {
+    events: [],
+  };
+  const timerState = {
+    isActive: false,
+    createdAt: "Timer Not Set",
+    remainingTime: "Timer Not Set",
+    endsAt: "Timer Not Set",
+  };
+  const taskKeys = {
+    timeStamp: new Date(),
+    completed: false,
+    timerState,
+    history,
+  };
+
+  return useMutation({
+    mutationKey: ["tasks"],
+    mutationFn: (newTask) => {
+      addDoc(collection(auth.database, "tasks"), {
+        ...newTask,
+        ...taskKeys,
+      });
+    },
+  });
+};
+
+export default useAddTasks;
